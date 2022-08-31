21fe7c8615e193fc58a69c1506bc8ef5234d38fe
srH-hype <jejeje300@gmail.com>
2022-08-31 16:43:02 -0600

    commit

---
+++ Ejemplos.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+func _ready():
+	ejercicio_2()
+
+func ejercicio_1():
+	print("algo")
+
+func ejercicio_2():
+	var variablePalabra = "Palabra"
+	var variableNumero = 777
+	var variableNumeroDesimal = 7.77
+	var variableMentira = false
+	
+	print(variablePalabra)
+	print(variableNumero)
+	print(variableNumeroDesimal)
+	print(variableMentira)
+	
